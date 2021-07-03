cb48e4593df4e59c6af47de542f9af7550e440fb
SD <harrypotter.starwars.shrish@gmail.com>
2021-07-03 17:06:30 +0530

    :b:ixin stuff

---
+++ modules/interaction-events/src/main/java/net/legacyfabric/fabric/mixin/event/interaction/ServerPlayerInteractionManagerMixin.java
@@ -0,0 +1,9 @@
+package net.legacyfabric.fabric.mixin.event.interaction;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.server.network.ServerPlayerInteractionManager;
+
+@Mixin(ServerPlayerInteractionManager.class)
+public class ServerPlayerInteractionManagerMixin {
+}
